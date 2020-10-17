5d4d8d24c2a9be4fefbb13379b49b9e1441c26c2
Eugeniy Bekanurovich <passivevoice3@gmail.com>
2020-10-17 19:39:58 +0300

    feat: add solution for last two tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if(!Array.isArray(arr)) throw Error('Wrong type');
+    let degreeDeep = 1;
+
+    for (let elem of arr) {
+      if (Array.isArray(elem)) {
+        let deep = this.calculateDepth(elem) + 1;
+
+        if(degreeDeep < deep) degreeDeep = deep;
+      }
+    }
+
+    return degreeDeep;
   }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw Error('Missing argument');
+
+    message =  message.toLowerCase();
+    key = key.toLowerCase();
+  
+    let result = '';
+
+    for(let i = 0; i < message.length; i++) {
+      if (message.length > key.length) {
+        key = `${key}${key[i]}`
+      }
+    }
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let c = message.charCodeAt(i);
+      let k = key.charCodeAt(j);
+
+      if (97 <= c && c <= 122) {
+        result += String.fromCharCode(((c - 97) + (k - 97)) % 26 + 97);
+        j++; // so as not to switch to a new letter in key
+      } else {
+        result +=  String.fromCharCode(c);
+      }
+    }
+    
+    return this.mode === true ? result.toUpperCase() : result.split('').reverse().join('').toUpperCase();
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(message, key) {
+    if(!message || !key) throw Error('Missing argument');
+    
+    message =  message.toLowerCase();
+    key = key.toLowerCase();
+
+    let result = '';
+
+    for(let i = 0; i < message.length; i++) {
+      if (message.length > key.length) {
+        key = `${key}${key[i]}`
+      }
+    }
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let c = message.charCodeAt(i);
+      let k = key.charCodeAt(j);
+
+      if (97 <= c && c <= 122) {
+        result += String.fromCharCode((c + 26 - k)  % 26 + 97);
+        j++; // so as not to switch to a new letter in key
+      } else {
+        result +=  String.fromCharCode(c);
+      }
+    }
+    
+    return this.mode === true ? result.toUpperCase() : result.split('').reverse().join('').toUpperCase();
   }
 }
 
